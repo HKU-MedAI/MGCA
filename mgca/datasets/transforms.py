b9ec84f378e9561628c30f39d5ee64250f450f37
Fuying Wang <thuwfy15@gmail.com>
2023-02-24 11:50:02 +0800

    fix bug of moco2transform

--- mgca/datasets/transforms.py
+++ mgca/datasets/transforms.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import torchvision.transforms as transforms
+import random
+from PIL import ImageFilter
 
 
 class DataTransforms(object):
@@ -52,32 +54,6 @@ class DetectionDataTransforms(object):
 
     def __call__(self, image):
         return self.data_transforms(image)
-
-
-class GaussianBlur:
-    # Implements Gaussian blur as described in the SimCLR paper
-    def __init__(self, kernel_size, p=0.5, min=0.1, max=2.0):
-
-        self.min = min
-        self.max = max
-
-        # kernel size is set to be 10% of the image height/width
-        self.kernel_size = kernel_size
-        self.p = p
-
-    def __call__(self, sample):
-        sample = np.array(sample)
-
-        # blur the image with a 50% chance
-        prob = np.random.random_sample()
-
-        if prob < self.p:
-            sigma = (self.max - self.min) * \
-                np.random.random_sample() + self.min
-            sample = cv2.GaussianBlur(
-                sample, (self.kernel_size, self.kernel_size), sigma)
-
-        return sample
 
 
 class Moco2Transform(object):
